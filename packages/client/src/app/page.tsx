7b49fcdd9c215c8cc3fd475ba600fb691ee6edc4
Drago96 <dragproychev@gmail.com>
2023-06-19 23:01:19 +0300

    Small wording change

--- packages/client/src/app/page.tsx
+++ packages/client/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-5">
       <Typography variant="h2" className="text-5xl font-bold">
-        Buy and sell Assets in minutes
+        Buy & sell Assets in minutes
       </Typography>
       <Typography>Join the world&apos;s largest asset exchange</Typography>
     </div>
