32e87a8d4f0c4f7fad5fab4c34c0e0131b9da9c2
Drago96 <dragproychev@gmail.com>
2023-06-30 14:51:24 +0300

    Adjust styles of page headings

--- packages/client/src/app/(user-profile)/wallet/page.tsx
+++ packages/client/src/app/(user-profile)/wallet/page.tsx
@@ -2,7 +2,7 @@ import Typography from "@/components/common/typography";
 
 export default function Wallet() {
   return (
-    <Typography variant="h2" className="text-5xl font-bold">
+    <Typography variant="h2" className="test-3xl font-bold lg:text-5xl">
       Wallet
     </Typography>
   );
--- packages/client/src/app/page.tsx
+++ packages/client/src/app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
   return (
     <div className="flex flex-col gap-5 lg:gap-20">
       <div className="flex flex-col gap-5">
-        <Typography variant="h2" className="text-5xl font-bold">
+        <Typography variant="h2" className="text-3xl font-bold lg:text-5xl">
           Buy & sell Assets in minutes
         </Typography>
         <Typography>Join the world&apos;s largest asset exchange</Typography>
