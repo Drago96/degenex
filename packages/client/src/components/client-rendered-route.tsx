e1f38b4797ea245f74edfddd9fee1551ed784c07
Drago96 <dragproychev@gmail.com>
2023-08-07 18:45:23 +0300

    Fix next build error

--- packages/client/src/components/client-rendered-route.tsx
+++ packages/client/src/components/client-rendered-route.tsx
@@ -1,19 +1,19 @@
-import { FC, PropsWithChildren } from "react";
+import { FC } from "react";
 
 import { useIsHydrated } from "@/hooks/use-is-hydrated";
 import Loading from "@/app/loading";
 
-export default function ClientRenderedRoute<PropsT extends PropsWithChildren>(
-  WrappedComponent: FC<PropsT>
+export default function ClientRenderedRoute(
+  WrappedComponent: FC
 ) {
-  function ClientRenderedComponent(props: PropsT) {
+  function ClientRenderedComponent() {
     const isHydrated = useIsHydrated();
 
     if (!isHydrated) {
       return <Loading />;
     }
 
-    return <WrappedComponent {...props} />;
+    return <WrappedComponent />;
   }
 
   ClientRenderedComponent.displayName = WrappedComponent.displayName;
